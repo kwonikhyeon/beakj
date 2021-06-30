8671881f80c40d1cfca6e99db777b1c8c031f28e
kwonikhyeon <kwen1028@naver.com>
2021-07-01 00:23:34 +0900

    Update 1012.py

--- 1012.py
+++ 1012.py
@@ -1,10 +1,26 @@
 import sys
 
-jirung1, jirung2 = 0
+def check(m,n,po,o):
+    if (o[0]+1,o[1]) in po: 
+        po.remove(o[0]+1,o[1])
+        check(m,n,po,(o[0]+1,o[1]))
+    elif (o[0]-1,o[1]) in po: 
+        check(m,n,po,(o[0]-1,o[1]))
+    elif (o[0],o[1]+1) in po: 
+        check(m,n,po,(o[0],o[1]+1))
+    elif (o[0],o[1]-1) in po: 
+        check(m,n,po,(o[0],o[1]-1))
+
+
+jirung1 = 0 
+jirung2 = 0
 p=[]
 num = int(sys.stdin.readline())
 for i in range(num):
     m, n, c = map(int, sys.stdin.readline().split())
     for j in range(c):
         p.append(list(map(int, sys.stdin.readline().split())))
-
+    for k in range(m):
+        for l in range(n):
+            if (k,l) in p: check(m,n,p,(k,l))
+                
