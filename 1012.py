255a86e7823213934b97c90dab70b8321ee869d4
kwonikhyeon <kwen1028@naver.com>
2021-07-01 16:03:47 +0900

    Update 1012.py

--- 1012.py
+++ 1012.py
@@ -1,26 +1,28 @@
 import sys
+sys.setrecursionlimit(100000)
 
-def check(m,n,po,o):
-    if (o[0]+1,o[1]) in po: 
-        po.remove(o[0]+1,o[1])
-        check(m,n,po,(o[0]+1,o[1]))
-    elif (o[0]-1,o[1]) in po: 
-        check(m,n,po,(o[0]-1,o[1]))
-    elif (o[0],o[1]+1) in po: 
-        check(m,n,po,(o[0],o[1]+1))
-    elif (o[0],o[1]-1) in po: 
-        check(m,n,po,(o[0],o[1]-1))
+def check(m,n,k,l):
+    if [k+1,l] in p: check(m,n,k+1,l)
+    elif [k-1,l] in p: check(m,n,k-1,l)
+    elif [k,l+1] in p: check(m,n,k,l+1)
+    elif [k,l-1] in p: check(m,n,k,l-1)
+
+    p.remove([k,l])
 
 
-jirung1 = 0 
-jirung2 = 0
+
 p=[]
 num = int(sys.stdin.readline())
+jirung1 = [0]*num
 for i in range(num):
     m, n, c = map(int, sys.stdin.readline().split())
     for j in range(c):
         p.append(list(map(int, sys.stdin.readline().split())))
     for k in range(m):
         for l in range(n):
-            if (k,l) in p: check(m,n,p,(k,l))
+            if [k,l] in p: 
+                check(m,n,k,l)
+                jirung1[i] += 1
+
+for i in range(num): print(jirung1[i])
                 
