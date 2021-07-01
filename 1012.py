24d953b8990bf159bf8e34d04e9f8c88f699d283
kwonikhyeon <kwen1028@naver.com>
2021-07-02 00:51:56 +0900

    Update 1012.py

--- 1012.py
+++ 1012.py
@@ -1,19 +1,17 @@
-import sys
-sys.setrecursionlimit(100000)
 
-def check(m,n,k,l):
-    if [k+1,l] in p: check(m,n,k+1,l)
-    elif [k-1,l] in p: check(m,n,k-1,l)
-    elif [k,l+1] in p: check(m,n,k,l+1)
-    elif [k,l-1] in p: check(m,n,k,l-1)
+import sys
+
+def check(k,l):
+    if [k+1,l] in p: check(k+1,l)
+    elif [k-1,l] in p: check(k-1,l)
+    elif [k,l+1] in p: check(k,l+1)
+    elif [k,l-1] in p: check(k,l-1)
 
     p.remove([k,l])
-
-
-
+'''
 p=[]
 num = int(sys.stdin.readline())
-jirung1 = [0]*num
+jirung = [0]*num
 for i in range(num):
     m, n, c = map(int, sys.stdin.readline().split())
     for j in range(c):
@@ -21,8 +19,19 @@ for i in range(num):
     for k in range(m):
         for l in range(n):
             if [k,l] in p: 
-                check(m,n,k,l)
-                jirung1[i] += 1
+                #check(k,l)
+                jirung[i] += 1
+    p.clear()
 
-for i in range(num): print(jirung1[i])
-                
+for i in range(num): print(jirung[i])
+'''
+
+num = int(sys.stdin.readline())
+jirung = [0]*num
+for i in range(num):
+    m, n, c = map(int, sys.stdin.readline().split())
+    p = [[0]*m for i in range(n)]
+    for j in range(c):
+        a,b = int(sys.stdin.readline().split())
+        p[b][a] = 1
+    
